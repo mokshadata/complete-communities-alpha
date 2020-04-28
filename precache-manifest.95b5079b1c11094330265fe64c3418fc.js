089ea264a572c569c67d18bd0c2af6ad584f54e4
Amanda Shih <pandafulmanda@gmail.com>
2020-04-28 09:37:56 -0500

    Updates

---
+++ precache-manifest.95b5079b1c11094330265fe64c3418fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b4faf41e727d436bdcbd",
+    "url": "/complete-communities-alpha/static/js/runtime~main.b4faf41e.js"
+  },
+  {
+    "revision": "f94dc51f7f5900d901be",
+    "url": "/complete-communities-alpha/static/js/main.f94dc51f.chunk.js"
+  },
+  {
+    "revision": "62efc99321fce30a61bc",
+    "url": "/complete-communities-alpha/static/js/2.62efc993.chunk.js"
+  },
+  {
+    "revision": "f94dc51f7f5900d901be",
+    "url": "/complete-communities-alpha/static/css/main.1c6043c0.chunk.css"
+  },
+  {
+    "revision": "62efc99321fce30a61bc",
+    "url": "/complete-communities-alpha/static/css/2.c6a8c688.chunk.css"
+  },
+  {
+    "revision": "cbd27950436e65ae2008508ff108bb8c",
+    "url": "/complete-communities-alpha/index.html"
+  }
+];
